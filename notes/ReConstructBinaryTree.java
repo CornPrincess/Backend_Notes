873b218eed12964e181d1b2cd7834d1ac5eef982
cornprinicess <xiaocorn96@gmail.com>
2020-02-21 23:38:57 +0800

    fix reConstructBinaryTree

--- notes/ReConstructBinaryTree.java
+++ notes/ReConstructBinaryTree.java
@@ -18,11 +18,20 @@ public class Solution {
         for (int i = 0; i < in.length; i++) {
             indexForInOrders.put(in[i], i);
         }
-        return reConstructBinaryTree(pre, 0, pre.length - 1, 0);K
+        return reConstructBinaryTree(pre, 0, pre.length - 1, 0);
     }
 
     private TreeNode reConstructBinaryTree(int[] pre, int preL, int preR, int inL) {
+        if (preL > preR) {
+            return  null;
+        }
 
+        TreeNode root = new TreeNode(pre[preL]);
+        int inIndex = indexForInOrders.get(root.val);
+        int leftTreeSize = inIndex - inL;
+        root.left = reConstructBinaryTree(pre, preL + 1, preL + leftTreeSize, inL);
+        root.right = reConstructBinaryTree(pre, preL + leftTreeSize + 1, preR, inL + leftTreeSize + 1 );
+        return root;
     }
 
 }
